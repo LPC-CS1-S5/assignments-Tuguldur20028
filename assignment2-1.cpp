29ce87b873edb17bd95b2da9ceb0cecf239d3419
Tuguldur20028 <milkeytuguldur@gmail.com>
2021-01-27 16:10:03 +0000

    my assignments say missing but I did it last week I think I probably forgot to commit

---
+++ assignment2-1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int checking;
+  unsigned int miles;
+  long days;
+
+  checking = -20;
+  miles = 4276;
+  days = 18900;
+  cout <<"We have made a long journey of" << miles << "miles.\n";
+  cout <<"Our checking account balance is" << checking<<"\n";
+  cout <<"\n About" << days << "Days ago Columbus";
+  cout <<"stood on this spot.\n";
+
+  return 0;
+}
