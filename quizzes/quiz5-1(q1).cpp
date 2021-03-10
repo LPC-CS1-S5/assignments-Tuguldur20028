7de8d108c995972075ef9df64e144c18d914d847
Tuguldur20028 <milkeytuguldur@gmail.com>
2021-03-10 17:33:57 +0000

    question1

--- quizzes/quiz5-1(q1).cpp
+++ quizzes/quiz5-1(q1).cpp
@@ -22,7 +22,21 @@ int main()
 	// }
    cout << "RAND_MIN value is " << RAND_MIN << endl;
   cout << num1 <<", " num2 <<", " num3 <<", " num4 <<", " num5 <<", ";
-  if(num1 <= num2 & num1 <= num3 & num1 <= num4 & num1 <= num5 );
+  if(num1 <= num2 & num1 <= num3 & num1 <= num4 & num1 <= num5 );{
+    cout << num1 <<"is smallest"
+  }
+  else if(num2 < num1 & num2 < num3 & num2 < num4 & num2 < num5 );{
+    cout << num2 <<"is smallest"
+  }
+  else if(num3 < num1 & num3 < num2 & num3 < num3 & num3 <num5 );{
+    cout << num3 <<"is smallest"
+  }
+  else if(num4 < num1 & num4 < num2 & num4 < num3& num4 < num5 );{
+    cout << num4 <<"is smallest"
+  }
+  else if(num5 < num1 & num5 < num2 & num5 < num3 & num5 < num4 );{
+    cout << num5 <<"is smallest"
+  }
   cout<<"The Smallest random number is: "<<smallest;
 }
   return 0;
