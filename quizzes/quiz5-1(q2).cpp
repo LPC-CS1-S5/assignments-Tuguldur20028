a4a4cb2c667990093dfaed92e71617c349d25446
Tuguldur20028 <milkeytuguldur@gmail.com>
2021-03-10 18:00:13 +0000

    q2

---
+++ quizzes/quiz5-1(q2).cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main()
+{
+  int n, m, power;
+  int i = 0;
+
+  do{
+    cout << "Please enter a number and the power";
+
+    cin >> n >> m;
+    cout << endl;
+    if (0 < n && n > 10 && 1 > m && m > 10);
+      check = true;{
+      while(check==false);
+    while(i < m){
+        power = n*i;
+        cout << power << endl;
+      }
+    } 
+  }
+
+}
