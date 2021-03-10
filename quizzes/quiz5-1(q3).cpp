f6231b20cb64d4bd08a6d9568ca43440ba095e44
Tuguldur20028 <milkeytuguldur@gmail.com>
2021-03-10 17:49:50 +0000

    q3

---
+++ quizzes/quiz5-1(q3).cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<cmath>
+#include<cstdlib>
+#include<ctime>
+
+void primeInRange(int L, int R) 
+{ 
+    int i, j, flag; 
+  
+    
+    for (i = L; i <= R; i++) { 
+  
+        
+        if (i == 1 || i == 0) 
+            continue; 
+  
+        
+        flag = 1; 
+  
+        
+        for (j = 2; j <= i / 2; ++j) { 
+            if (i % j == 0) { 
+                flag = 0; 
+                break; 
+            } 
+        } 
+  
+       
+        if (flag == 1) 
+            printf("%d ", i); 
+    } 
+} 
+  
+
+int main() 
+{ 
+    
+    int L = 1; 
+    int R = 10; 
+  
+    
+    primeInRange(L, R); 
+  
+    return 0; 
+}
