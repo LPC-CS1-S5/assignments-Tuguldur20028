a4b92b2fb652e97e48e1eadb62edcbe20a473168
Tuguldur20028 <milkeytuguldur@gmail.com>
2021-03-10 17:23:22 +0000

    1q

--- quizzes/quiz5-1(q1).cpp
+++ quizzes/quiz5-1(q1).cpp
@@ -6,10 +6,23 @@ using namespace std;
 int main()
 {
   srand(time(0));
+  
+  // Use the for loop to generate random numbers
+
+  int num = rand () % 100;
   int num1 = rand () % 100;
   int num2 = rand () % 100;
   int num3 = rand () % 100;
   int num4 = rand () % 100;
   int num5 = rand () % 100;
 
-  
+	// for (....) {
+	//     num = rand() % 100;
+	//     Here, you need some code to find min value.
+	// }
+   cout << "RAND_MIN value is " << RAND_MIN << endl;
+  cout << num1 <<", " num2 <<", " num3 <<", " num4 <<", " num5 <<", ";
+  if(num1 <= num2 & num1 <= num3 & num1 <= num4 & num1 <= num5 );
+  cout<<"The Smallest random number is: "<<smallest;
+}
+  return 0;
