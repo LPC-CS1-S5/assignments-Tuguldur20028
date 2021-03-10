93f429b7269ba5b7ce771d909cf1c724d7d51b51
Tuguldur20028 <milkeytuguldur@gmail.com>
2021-03-10 16:51:31 +0000

    quizzes first 10 min

---
+++ quizzes/quiz5-1(q1).cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cmath>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+int main()
+{
+  srand(time(0));
+  int num1 = rand () % 100;
+  int num2 = rand () % 100;
+  int num3 = rand () % 100;
+  int num4 = rand () % 100;
+  int num5 = rand () % 100;
+
+  
