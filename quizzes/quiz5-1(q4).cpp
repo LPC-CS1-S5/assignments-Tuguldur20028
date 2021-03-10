3ab646a1d99d2d2f96b11692ebb6dae39e50c573
Tuguldur20028 <milkeytuguldur@gmail.com>
2021-03-10 18:32:05 +0000

    q4

---
+++ quizzes/quiz5-1(q4).cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<cmath>
+#include<cstdlib>
+#include<ctime>
+
+using namespace std;
+int main(){
+    string Employee ID;
+    string Employee Name;
+    string Department Name;
+    int Salary;
+
+ofstream ofs;
+ofs.open("employee.txt");
+
+cout << "How many Employee";
+cin >> numEmployee;
+
+int i=0;
+    i << numEmployee;
+    i++ ;
+    cout << " what is employee's number";
+    cin >> numEmployee;
+    cout << " what is employee's department";
+    cin >> department;
+    cout << " what is employee's name";
+    cin >> nameEmployee;
+    cout << " what is employee's salary";
+    cin >> salary;
+  
+  ofs.close;
+}
+
+  return 0;
+
+
+
+
+
+
+
+
