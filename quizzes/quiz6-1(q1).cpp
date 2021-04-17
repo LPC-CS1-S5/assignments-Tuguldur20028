579c0737756a071eef35897a7c01d61f834717d4
Kyuwoong Lee <>
2021-04-17 04:13:25 +0000

    KYU

--- quizzes/quiz6-1(q1).cpp
+++ quizzes/quiz6-1(q1).cpp
@@ -1,3 +1,50 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+// //Function prototypes
+// void swapTwoValues(int &n1, int &n2);
+// void swapTwoValues (float &f1, float &f2);
+
+// int main()
+// {
+//     int n1, n2;
+//     float f1, f2;
+
+//     //Get user input
+//     cout << fixed << showpoint << setprecision(2);
+//     cout << "Enter two integers \n";
+//     cin >> n1 >> n2;
+//     cout <<"Enter two floating point values \n";
+//     cin >> f1 >>f2;
+
+//     cout << "Befor Swap int "<<setw(9)<<n1<<setw(9)<< n2<<endl;
+//     swapTwoValues( n1,n2);
+//     cout << "After Swap int "<<setw(9)<<n1<<setw(9)<< n2<<endl;
+
+
+//     cout << "Befor Swap float "<<setw(7)<<f1<<setw(7)<< f2<<endl;
+//     swapTwoValues( f1,f2);
+//     cout << "After Swap float"<<setw(7)<<f1<<setw(7)<< f2<<endl;
+
+// }
+// // Definition of overloaded function
+// // this function uses an reference parameter int n1, n2.
+// void swapTwoValues ( int &n1, int &n2)
+// {
+//     int temp;
+//     temp = n1;
+//     n1 = n2;
+//     n2 = temp;
+// }
+// void swapTwoValues ( float &n1, float &n2)
+// {
+//     float temp;
+//     temp = n1;
+//     n1 = n2;
+//     n2 = temp;
+// }
+
 #include	<iostream>
 #include	<iomanip>
 using namespace	std;
--- quizzes/quiz6-2(q2).cpp
+++ quizzes/quiz6-2(q2).cpp
@@ -1,3 +1,59 @@
+// #include <iostream>
+// #include <iomanip>
+// #include <ctime>
+// using namespace std;
+
+// //Function prototypes
+// void getRandnum(int &n1, int &n2, int &n3);
+// int findMin(int , int , int);
+// void printResult(int n1, int n2,int n3, int min);
+
+
+// int main()
+// {
+//     int n1,n2,n3,min;
+
+//     getRandnum(n1,n2, n3);
+//     min=findMin(n1,n2,n3);
+//     printResult(n1,n2, n3, min);
+//     return 0;
+// }
+
+// //Function definition
+// // Generate three random numbers and pass them by reference.
+// void getRandnum (int &n1, int &n2, int &n3)
+// {
+//   unsigned seed = time(0);
+//   srand (seed);
+//   n1 = rand() % 100;
+//   n2 = rand() % 100;
+//   n3 = rand() % 100;
+
+// }
+
+// //value returning function
+// // it finds a min among 3 numbers and returns min
+// int findMin(int n1, int n2, int n3)
+// {
+//   int m;
+//   m = n1;
+//   if (m > n2)
+//     m = n2;
+//   if (m > n3)
+//     m = n3;
+//   return m;
+
+// }
+
+// //get the variables values from previos functions. pass by value parameters.
+// // print
+// void printResult (int n1, int n2,int n3, int min)
+// {
+//     cout << "The 3 random numbers are: \n";
+//     cout << n1<< " "<<n2 << " " << n3 << " "<<endl;
+//     cout << "The min is: "<< min<< endl;
+// }
+
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
