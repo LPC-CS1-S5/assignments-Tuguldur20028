7428e80e5f9695da14199b8014df83a814b8d921
Tuguldur20028 <>
2021-05-05 03:45:43 +0000

    7-1

--- assignment7-1.cpp
+++ assignment7-1.cpp
@@ -1,4 +1,4 @@
-//template
+#include <iostream>
 #include    <ctime>
 #include    <cstdlib>
 #include    <iomanip>
@@ -13,21 +13,34 @@ const   int     SIZE = 100;
 
 int         main()
 {
-    int     number[SIZE];
+    int     number[SIZE] = {10, 20, 30, 40, 50};
     int     last;
-
-    last = makearray(number);
+    last = 5;
     printout(number, last);
-    bubble(number, last);
+
+    for(int i=0; i<last-1; i++){
+      swap(number[i], number[i+1]);
+    }
     printout(number, last);
 }
 
-int     makearray(int   number[])
+int     makearray(int   number[], int SIZE)
 {
+    int last;
+    srand(time(NULL));
+    last = rand() % 10 + 10;
+    {
+      for(int i = 0; i < last; i++)
+      number[i] = rand() % 100;
+    return last;
+    }
 }
-void    bubble(int number[], int last)
-{
-}
+
 void    printout(int number[], int last)
 {
+  cout << "Array Size : " << last << endl;
+  for(int i = 0; i < last; i++){
+    cout << number[i]<< " ";
+  }
+  cout << endl;
 }
---
+++ quizzes/quiz7/quiz7-4.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+const int N = 10;
+
+int main(){
+  int number [N] = {10, 15, 20, 25, 0, 40, 35, 45, 30, 5};
+  for(int i = 0; i<[N]; i++){
+    if(i>0 && i<9){
+      if(abs(number[i]-number[i+1]<10 && abs (number [i]-number[i-1])<10)){
+        
+      }
+    }
+  } 
+}
