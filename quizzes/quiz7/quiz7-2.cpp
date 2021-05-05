345ba8ebbae6d1c8eeac1bad4b9a08f13439a7da
Tuguldur20028 <>
2021-05-05 03:57:27 +0000

    7-2

---
+++ quizzes/quiz7/quiz7-2.cpp
@@ -0,0 +1,16 @@
+#include  <iostream>
+using namespace std;
+
+void printtriangle(int numbers[][3],int);
+int main()
+{
+  const int SIZE = 3;
+  int numbers[SIZE][SIZE] = { {0, 1, 2}, {3, 4, 5}, {6, 7, 8} };//creating 2D array with values
+    
+    printtriangle(numbers, SIZE);//Calls the printtriangle function
+}
+void printtriangle(int num[][3], int size)
+{
+   for(int i = 0; i < size)
+}
+}
