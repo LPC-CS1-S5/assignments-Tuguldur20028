15a0cd15177aa1cb1fbf2d78eb6cce21f07510cd
Tuguldur20028 <>
2021-05-19 03:20:50 +0000

    quiz9

---
+++ quiz9/Question.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+
+using namespace std;
+
+struct employeeRecords{
+  int id;
+  string name;
+  int salary;
+  string department;
+  string date;
+};
+
