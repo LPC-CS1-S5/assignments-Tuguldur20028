0cbba4197a49def06d46082c966517328ad51319
Tuguldur20028 <>
2021-05-19 03:28:58 +0000

    quiz9.2

--- quiz9/Question.cpp
+++ quiz9/Question.cpp
@@ -12,3 +12,19 @@ struct employeeRecords{
   string date;
 };
 
+void filewrite();
+void	printrecord(const employeeRecords e);
+void employeestruct(employeeRecords e[]);
+void finddepartment(employeeRecords e[], int numRecords, string department);
+void findsalary(employeeRecords e[], int numRecords, int salary);
+
+int main(){
+  int numRecords = 0;
+  employeeRecords e[20];
+  int salary;
+  string department;
+
+  cout << "welcome to the programm reads employee imformation";
+  cout << "listing the employee who's salaries are above 100000.";
+  cout << "Find the employee that works in the Computer department.";
+  
