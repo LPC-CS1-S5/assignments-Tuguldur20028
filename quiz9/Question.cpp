53d52e253466b3da3f47c2f0769756a17c3b2be2
Tuguldur20028 <>
2021-05-19 04:03:53 +0000

    quiz9

--- quiz9/Question.cpp
+++ quiz9/Question.cpp
@@ -52,3 +52,42 @@ void filewrite(){
 	ofs << er.department << endl;
 	ofs << er.date << endl;
 
+ cout << "Your information was "<< sizeof(er)<< "bytes of data.\n";
+}
+
+void employeestruct(employeeRecords e[]){
+  ifstream ifs;
+  int idx=0,j;
+
+  ifs.open("employee.txt");
+  if (ifs){
+    idx = 0;
+    ifs >> e[idx].id >> e[idx].name >> e[idx].salary >> e[idx].department >> e[idx].date;
+  }
+}
+
+void	printrecord(const employeeRecords e)
+{
+		cout << e.id << "\t" << e.name << "\t" << e.salary << "\t" << e.department << "\t"<< e.date;
+}
+
+void finddepartment(employeeRecords e[], int numRecords, string department){
+  int pos = 0;
+	
+	cout << "Matched Records with " << department << endl;
+	for(int i=0; i<numRecords;i++){
+		pos = e[i].department.find("computer");
+		if (pos!=string::npos)
+			printrecord(e[i]);
+}}
+
+void findsalary(employeeRecords e[], int numRecords, int salary){
+int pos = 0;
+cout << "Here are employee make 100000 or more";
+for (int i = 0; i< numRecords; i++){
+//   while e[i]er.salary <= 100000{
+//   while (e[i].salary <= 500000){
+  if (e[i].salary <= 500000){
+			printrecord(e[i]);}
+}
+}
